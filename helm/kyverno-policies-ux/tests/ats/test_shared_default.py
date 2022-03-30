4560b80efcfcf0624d2c2488aec7bcc38966bce3
Zach Stone <zach@giantswarm.io>
2022-03-30 13:59:33 +0200

    Remove heartbeat tests

--- helm/kyverno-policies-ux/tests/ats/test_shared_default.py
+++ helm/kyverno-policies-ux/tests/ats/test_shared_default.py
@@ -19,29 +19,5 @@ import logging
 LOGGER = logging.getLogger(__name__)
 
 @pytest.mark.smoke
-def test_silence_heartbeat_policy(silence) -> None:
-    """
-    test_silence_heartbeat_policy tests defaulting of an empty Silence to check for the negative Heartbeat matcher.
-
-    :param silence: Any Silence CR.
-    """
-    matchers = silence['spec']['matchers']
-
-    for matcher in matchers:
-        if matcher['name'] == "alertname":
-            assert (matcher['value'] == "Heartbeat" and not matcher['isRegex'] and not matcher['isEqual'])
-            return
-
-    pytest.fail("Heartbeat matcher is missing")
-
-@pytest.mark.smoke
-def test_silence_heartbeat_policy_with_existing_matchers(silence_with_matchers) -> None:
-    """
-    test_silence_heartbeat_policy tests defaulting of a Silence container a matcher to check for the negative Heartbeat matcher.
-
-    :param silence_with_matchers: Any Silence CR container a matcher.
-    """
-    matchers = silence_with_matchers['spec']['matchers']
-
-    assert (matchers[0]['value'] == "test" and matchers[0]['name'] == "test" and not matchers[0]['isRegex'] and not matchers[0]['isEqual'])
-    assert (matchers[1]['value'] == "Heartbeat" and matchers[1]['name'] == "alertname" and not matchers[1]['isRegex'] and not matchers[1]['isEqual'])
+def test_empty_test() -> None:
+    return
