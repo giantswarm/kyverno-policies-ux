aee7e1297879a6ff8821465c015891ec8a2107fd
Marian Steinbach <marian@giantswarm.io>
2022-06-02 14:47:37 +0200

    Add basic test

--- helm/kyverno-policies-ux/tests/ats/test_shared_default.py
+++ helm/kyverno-policies-ux/tests/ats/test_shared_default.py
@@ -1,7 +1,7 @@
 import sys
 sys.path.append('../../../tests')
 
-import yaml
+import json
 from functools import partial
 import time
 import random
@@ -9,8 +9,8 @@ import string
 import ensure
 from textwrap import dedent
 
-from ensure import silence
-from ensure import silence_with_matchers
+from ensure import cluster_name
+from ensure import kubernetes_cluster
 
 import pytest
 from pytest_kube import forward_requests, wait_for_rollout, app_template
@@ -19,5 +19,16 @@ import logging
 LOGGER = logging.getLogger(__name__)
 
 @pytest.mark.smoke
-def test_empty_test() -> None:
-    return
+def test_service_priority_cluster_label(kubernetes_cluster) -> None:
+    """
+    Checks whether our policy to prevent invalid 'giantswarm.io/service-priority' label
+    values is working.
+    """
+
+    raw = kubernetes_cluster.kubectl(f"label --overwrite=true clusters.cluster.x-k8s.io {cluster_name}", output="json")
+
+    #processed = json.loads(raw)
+
+    LOGGER.info(f"Output of 'kubectl label' command: {raw}")
+    
+    assert True == False
