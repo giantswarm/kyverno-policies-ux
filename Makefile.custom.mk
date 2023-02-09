0d439a6bbce33a33c56ac320f7b1d256a0d589d1
Franco <franco@giantswarm.io>
2023-02-09 08:12:39 -0300

    Update Kyverno URL

--- Makefile.custom.mk
+++ Makefile.custom.mk
@@ -36,7 +36,7 @@ tilt-up: ## Start Tilt
 # If you change kyverno version here remember to change it in the Tiltfile too
 .PHONY: install-kyverno
 install-kyverno:
-	kubectl create --context kind-$(KIND_CLUSTER_NAME) -f https://raw.githubusercontent.com/kyverno/kyverno/$(KYVERNO_VERSION)/definitions/release/install.yaml
+	kubectl create --context kind-$(KIND_CLUSTER_NAME) -f https://github.com/kyverno/kyverno/releases/download/$(KYVERNO_VERSION)/install.yaml
 	kubectl wait --context kind-$(KIND_CLUSTER_NAME) --for=condition=ready pod -l app=kyverno -nkyverno
 
 .PHONY: kind-get-kubeconfig
