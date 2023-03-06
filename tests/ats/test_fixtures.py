dd2220dd97b1f415f4120e83e32c92dcfe338c18
Jose Armesto <github@armesto.net>
2023-03-06 09:51:57 +0100

    Update kyverno in tests to 1.9.0

--- tests/ats/test_fixtures.py
+++ tests/ats/test_fixtures.py
@@ -8,7 +8,7 @@ import time
 LOGGER = logging.getLogger(__name__)
 
 # TODO: Can we take this from a central config to avoid repetition?
-KYVERNO_VERSION = "v1.8.5"
+KYVERNO_VERSION = "v1.9.0"
 
 @pytest.fixture(scope='module')
 def fixtures(kube_cluster: Cluster):
@@ -35,14 +35,16 @@ def fixtures(kube_cluster: Cluster):
     # Our policies
     LOGGER.info("Deploy Kyverno policies for the service-priority cluster label")
     ret = kube_cluster.kubectl("apply", filename="../../policies/ux/clusters-label-service-priority.yaml", output_format="json")
+    LOGGER.debug(f"Created cluster service priority policies result: {ret}")
     ret = kube_cluster.kubectl("apply", filename="../../policies/ux/organization-deletion-when-has-clusters.yaml", output_format="json")
-    LOGGER.debug(f"Created kyverno policies result: {ret}")
+    LOGGER.debug(f"Created organization deletion policies result: {ret}")
 
     # Create Organization namespace
-    LOGGER.info("Create namespace named 'org-giantswarm'")
+    LOGGER.info("Create namespaces named 'org-giantswarm' and 'org-empty'")
     ret = kube_cluster.kubectl("apply", filename="test-organization.yaml", output_format="json")
-    ret = kube_cluster.kubectl("apply", filename="test-empty-organization.yaml", output_format="json")
     LOGGER.debug(f"Created giantswarm organization and its namespace: {ret}")
+    ret = kube_cluster.kubectl("apply", filename="test-empty-organization.yaml", output_format="json")
+    LOGGER.debug(f"Created empty organization and its namespace: {ret}")
 
     # This block is commented because k8s v1.24 is needed to use '--subresource', and at the time of writing this, dats/dabs is broken and we can't specify which version to use.
     # Patch Organizations so that they contain their namespace on their status field, like organization-operator does
